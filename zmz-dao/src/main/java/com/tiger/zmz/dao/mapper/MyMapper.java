6706a7519ed769b2409badd93ae7129155a90d45
zhanghai <zhanghai2@midea.com>
2019-07-17 18:24:27 +0800

    修改生成

--- zmz-dao/src/main/java/com/tiger/zmz/dao/mapper/MyMapper.java
+++ zmz-dao/src/main/java/com/tiger/zmz/dao/mapper/MyMapper.java
@@ -1,11 +1,12 @@
 package com.tiger.zmz.dao.mapper;
 
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
  * @author zhanghai2
  * @Description: 通用Mapper
  * @date 2019-7-17
  */
-public interface ZmzMapper<T> extends Mapper<T> {
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
 }
