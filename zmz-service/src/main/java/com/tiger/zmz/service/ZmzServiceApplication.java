209f0bd2cb17cd5c35f02357ba4d58e126696801
zhanghai <zhanghai2@midea.com>
2019-07-22 19:01:00 +0800

    添加service模块

---
+++ zmz-service/src/main/java/com/tiger/zmz/service/ZmzServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tiger.zmz.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmzServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZmzServiceApplication.class, args);
+  }
+
+}
