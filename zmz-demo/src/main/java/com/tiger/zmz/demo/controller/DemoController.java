e01c6ba47f49b927527b515b63e8f0ceb7c3be6a
zhhisabugs <zhhisabugs@163.com>
2019-09-24 22:35:41 +0800

    两个路径

--- zmz-demo/src/main/java/com/tiger/zmz/demo/controller/DemoController.java
+++ zmz-demo/src/main/java/com/tiger/zmz/demo/controller/DemoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -18,7 +19,7 @@ import springfox.documentation.annotations.ApiIgnore;
 public class DemoController {
 
     @ApiIgnore
-    @RequestMapping("/")
+    @RequestMapping(value = {"/hello", "hi"}, method = RequestMethod.GET)
     public String index() {
         return "hello";
     }
